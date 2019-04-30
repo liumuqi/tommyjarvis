878dd4ce0e2f6da928d37994f5a9dc6cfed24cdc
muqi.lmq <muqi.lmq@autonavi.com>
2019-04-30 10:38:53 +0800

    update

--- javabase/src/main/java/origin/base/M2N.java
+++ javabase/src/main/java/origin/base/M2N.java
@@ -8,24 +8,21 @@ import java.math.BigInteger;
  * @Desc:
  **/
 public class M2N {
-    private static String template = "0123456789abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ!@#$%^&*()_+|~-=\\`[]{};:,./<>?";
-//    private static String template = "01";
+    private static String template = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
 
     static String transRadix(BigInteger num, int toRadix) {
         StringBuilder sb = new StringBuilder();
         BigInteger m = BigInteger.valueOf(toRadix);
-        if (num.intValue() == 0) {
-            return "0";
-        }
         while (num.intValue() != 0) {
-//            sb.append(chs[Integer.parseInt(String.valueOf(num % toRadix))]);
-            sb.append(template.charAt(Integer.parseInt(String.valueOf(num.mod(m)))));
+            BigInteger mod = num.mod(m);
+            int index = Integer.parseInt(String.valueOf(mod));
+            char c = template.charAt(index);
+            sb.append(c);
             num = num.divide(m);
         }
         return sb.reverse().toString();
     }
 
-    // 其他进制转为10进制，按权展开
     public static BigInteger N210(String number, int N) {
         char ch[] = number.toCharArray();
         int len = ch.length;
