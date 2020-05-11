8b7479f8254c7443e1e5fa780b593b64313d7615
liumuqi <399211328@qq.com>
2020-05-11 17:41:49 +0800

    update

--- javabase/src/main/java/origin/base/inherits/M.java
+++ javabase/src/main/java/origin/base/inherits/M.java
@@ -14,21 +14,21 @@ public class M {
     public static void main(String[] args) {
         TestA testA = new TestA();
         TestB testB  = new TestB();
-        System.out.println(testA.getPushDataCount().toString());
-        System.out.println(testB.getPushDataCount().toString());
+        System.out.println("10 testA:"+testA.getPushDataCount().toString());
+        System.out.println("10 testB:"+testB.getPushDataCount().toString());
         Test testA1 = testB;
-        System.out.println(testA1.getPushDataCount().toString());
+        System.out.println("10 testA1:"+testA1.getPushDataCount().toString());
 
         testA.setPd();
 
         testB.setPd();
 
-        System.out.println(testA.getPushDataCount().toString());
-        System.out.println(testB.getPushDataCount().toString());
+        System.out.println("333 testA:"+testA.getPushDataCount().toString());
+        System.out.println("111 testB:"+testB.getPushDataCount().toString());
 
-        System.out.println(testA1.getPushDataCount());
+        System.out.println("111 testA1 ref testB:"+testA1.getPushDataCount());
         TestA testAa = new TestA();
-        System.out.println(testAa.getPushDataCount());
+        System.out.println("10 testAa:"+testAa.getPushDataCount());
 
 
         Date yyyyMMdd = DateTimeFormat.forPattern("yyyyMMdd").parseDateTime("19831201").toDate();
